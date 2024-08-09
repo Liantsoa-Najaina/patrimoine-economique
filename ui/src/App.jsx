68e1c796f0f267e2030dfc89090b52e1c86641b5
Najaina <hei.najaina@gmail.com>
2024-08-09 22:16:16 +0300

    fix:remove unnecessary comments

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Title from "./components/Navbar.jsx";
 import TableauPossessions from "./components/TableauPossessions.jsx";
-// import CalculerPatrimoine from "./components/CalculerPatrimoine.jsx";
 
 function App() {
     return (
@@ -11,14 +10,8 @@ function App() {
                 <section className={"possessions"}>
                     <TableauPossessions/>
                 </section>
-{/*
-                <section className={"valeur-patrimoine"}>
-                    <h2>Valeur de votre patrimoine économique</h2>
-                    <CalculerPatrimoine></CalculerPatrimoine>
-                </section>*/}
             </main>
         </>
     )
 }
-
 export default App
