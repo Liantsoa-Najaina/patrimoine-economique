f4bd58a1acb82de5b14b4d127e8b3952108c0d80
Liantsoa-Najaina <hei.najaina@gmail.com>
2024-08-30 15:34:00 +0300

    chores: start backend integration with ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
-import Title from "./components/Navbar.jsx";
-import TableauPossessions from "./components/TableauPossessions.jsx";
 
 function App() {
     return (
         <>
-            <Title/>
-            <main className="main-container">
-                <section className={"possessions"}>
-                    <TableauPossessions/>
-                </section>
-            </main>
+
         </>
     )
 }
