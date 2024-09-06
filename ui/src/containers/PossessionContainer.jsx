7914aa16400b26c9170e6ee1a928871f0a4a2957
Liantsoa-Najaina <hei.najaina@gmail.com>
2024-09-06 15:27:05 +0300

    fix: separate dumb and smart component for the possession creation feature

--- ui/src/containers/PossessionContainer.jsx
+++ ui/src/containers/PossessionContainer.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import PossessionTable from "../components/PossessionTable.jsx";
+import PossessionCreationForm from "../components/PossessionCreationForm.jsx";
 
 const PossessionContainer = () => {
 	const [possessions, setPossessions] = useState([]);
 	const [error, setError] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await axios.get('/possession'); //
-				setPossessions(response.data);
-			} catch (err) {
-				setError(err.message);
-			}
-		};
+	const fetchPossessions = async () => {
+		try {
+			const response = await axios.get('/possession');
+			setPossessions(response.data);
+		} catch (err) {
+			setError(err.message);
+		}
+	};
 
-		fetchData();
+	useEffect(() => {
+		fetchPossessions();
 	}, []);
+
+	const handleCreatePossession = async (newPossession) => {
+		try {
+			await axios.post('/possession/create', newPossession);
+			fetchPossessions();
+		} catch (err) {
+			setError(err.message);
+		}
+	};
 
 	if (error) {
 		return <div>Error: {error}</div>;
@@ -26,6 +36,7 @@ const PossessionContainer = () => {
 	return (
 		<div>
 			<h2>Liste des Possessions</h2>
+			<PossessionCreationForm onCreatePossession={handleCreatePossession} />
 			<PossessionTable possessions={possessions} />
 		</div>
 	);
